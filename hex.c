fb8f9048d48c939b9aba740a2f54de69113cebd2
Danil Kolotushkin <socurahlem@yandex.ru>
2019-12-17 16:25:11 +0300

    Added borders for output table

--- hex.c
+++ hex.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 
 int main(int argc, char* argv[])
 {
+	struct winsize term;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &term);
+	int w = term.ws_col - 1;
 	if (argc != 2)
 	{
 		unsigned int counter = 0;
@@ -18,25 +23,53 @@ int main(int argc, char* argv[])
 			{
 				unsigned char sym;
 				fread(&sym, sizeof(char), 1, f);
-				printf("%x ", sym);			
+				if (sym < 16)
+				{
+					printf("0");
+				}
+				printf("%x ", sym);		
+				counter += 3;
+				if (counter >= w) 
+				{
+					counter = 0;
+					printf("\n");
+				}
 			}
 			else if (strcmp(argv[2],"-8") == 0)
 			{
 				char sym;
 				fread(&sym, sizeof(char), 1, f);
-				printf("%d ", sym);		
+				printf("%4d ", sym);	
+				counter += 5;
+				if (counter >= w) 
+				{
+					counter = 0;
+					printf("\n");
+				}
 			}
 			else if (strcmp(argv[2],"-u8") == 0)
 			{	
 				unsigned char sym;
 				fread(&sym, sizeof(char), 1, f);
-				printf("%d ", sym);
+				printf("%3d ", sym);
+				counter += 4;
+				if (counter >= w) 
+				{
+					counter = 0;
+					printf("\n");
+				}
 			}	
 			else if (strcmp(argv[2],"-sym") == 0)
 			{	
 				char sym;
 				fread(&sym, sizeof(char), 1, f);
 				printf("%c", sym);
+				counter += 1;
+				if (counter >= w) 
+				{
+					counter = 0;
+					printf("\n");
+				}
 			}
 			else
 			{
