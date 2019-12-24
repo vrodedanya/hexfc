5c341a04c5bec39d4a24dae9e38e65f3a09e7e73
Danil Kolotushkin <socurahlem@yandex.ru>
2019-12-24 23:47:32 +0300

    Added line numbering and changed some flags
    Flags that have been changed:
    -hex -> -h
    -sym -> -s
    Added new flag -r. It works like 'cat'

--- hex.c
+++ hex.c
@@ -7,20 +7,25 @@ int main(int argc, char* argv[])
 {
 	struct winsize term;
 	ioctl(STDOUT_FILENO, TIOCGWINSZ, &term);
-	int w = term.ws_col - 1;
-	if (argc != 2)
+	int w = term.ws_col - 10; // width of terminal
+	if (argc == 3)
 	{
-		unsigned int counter = 0;
 		FILE* f;
 		if ((f = fopen(argv[1],"rb+")) == NULL)
 		{
 			fprintf(stderr,"Opening file error!\n");
 			return 0;
 		}
+		unsigned int counter = 0;
+		unsigned int number = 0;
 		while(!feof(f))
 		{
-			if (strcmp(argv[2],"-hex") == 0) 
+			if (strcmp(argv[2],"-h") == 0) 
 			{
+				if (counter == 0)
+				{
+					printf("%6d| ", number++);
+				}
 				unsigned char sym;
 				fread(&sym, sizeof(char), 1, f);
 				if (sym < 16)
@@ -37,10 +42,15 @@ int main(int argc, char* argv[])
 			}
 			else if (strcmp(argv[2],"-8") == 0)
 			{
+				if (counter == 0)
+				{
+					printf("%6d| ", number++);
+				}
+
 				char sym;
 				fread(&sym, sizeof(char), 1, f);
 				printf("%4d ", sym);	
-				counter += 5;
+				counter += 6;
 				if (counter >= w) 
 				{
 					counter = 0;
@@ -49,39 +59,62 @@ int main(int argc, char* argv[])
 			}
 			else if (strcmp(argv[2],"-u8") == 0)
 			{	
+				if (counter == 0)
+				{
+					printf("%6d| ", number++);
+				}
+
 				unsigned char sym;
 				fread(&sym, sizeof(char), 1, f);
 				printf("%3d ", sym);
-				counter += 4;
+				counter += 5;
 				if (counter >= w) 
 				{
 					counter = 0;
 					printf("\n");
 				}
 			}	
-			else if (strcmp(argv[2],"-sym") == 0)
+			else if (strcmp(argv[2],"-s") == 0)
 			{	
+				if (counter == 0)
+				{
+					printf("%6d| ", number++);
+				}
+
 				char sym;
 				fread(&sym, sizeof(char), 1, f);
-				printf("%c", sym);
-				counter += 1;
+				if (sym != 10)
+				{
+					printf("%c ", sym);
+				}
+				else
+				{
+					counter = w + 1;
+				}
+				counter += 2;
 				if (counter >= w) 
 				{
 					counter = 0;
 					printf("\n");
 				}
 			}
+			else if (strcmp(argv[2],"-r") == 0)
+			{
+				char sym;
+				fread(&sym, sizeof(char), 1, f);
+				printf("%c", sym);
+			}
 			else
 			{
-				fprintf(stderr, "Incorrect input!\nhexfc <path> -flag\n");
+				fprintf(stderr, "Wrong flag!\n");
 				return 0;
 			}
 		}
-		printf("\n");
 	}
 	else
 	{
-		fprintf(stderr, "Incorrect input!\nhexfc <path> -flag\n");
+		fprintf(stderr, "Incorrect input!\nhexfc <path> -flag");
 	}
+	printf("\n");
 	return 0;
 }
